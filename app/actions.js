8f3f0a82f119770fe3302b5a6fe3e13da28d91a4
Ronil Mehta <ronilvmehta@gmail.com>
2016-11-22 15:02:34 -0500

    Added logic to stream data chunks to 'stream' action

--- app/actions.js
+++ app/actions.js
@@ -9,11 +9,25 @@ module.exports = (_config) => {
 			//var urlObj = url.parse(`http://${request.url}`);
 			response.end("Index");
 
+		},
+		stream: (request,response) => {
+			response.connection.setTimeout(0);
+
+			var body = [];
+			request.on('data', function(chunk) {
+			  body.push(chunk);
+			}).on('end', function() {
+			  body = Buffer.concat(body).toString();
+			  	console.log(body); //This is the video data
+			});
+
+
+
 		}
-
-
 	}
 
 
 
 }
+
+//ffmpeg -i Carousselle.mp4 -f mpeg1video http://localhost:4321/stream
